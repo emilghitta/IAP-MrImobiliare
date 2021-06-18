3db8fb9cbb8f4d97901d8878e972045b7733f36e
emilghitta <emilghitta@yahoo.com>
2021-06-18 23:43:13 +0300

    Fixes #1 Mesajul de credentiale invalide a fost adaugat. El este handled in mod dinamic atat din fisierul logare.php care ne ajuta sa primim un parametru in URL, parametru care o sa fie handled in fisierul custom.js prin interogarea parametrului iar in cazul in care valoarea parametrului este failed v-om face display la eroare. Algoritmul a fost implementat cu ajutorul unei clase custom ce ne lasa sa selectam header-ul si div-ul de eroare cu ajutorul xpath-ului.

--- Conectare.php
+++ Conectare.php
@@ -79,6 +79,6 @@
   <script src="js/scripts.min.js"></script>
   <script src="js/main.min.js"></script>
   <script src="js/custom.js"></script>
-
+    
   </body>
 </html>
--- js/custom.js
+++ js/custom.js
@@ -9,26 +9,29 @@ function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-var errorDiv = getElementByXpath("/html/body/div[4]/div/div/div");
-var errorText = getElementByXpath("/html/body/div[4]/div/div/div/h4");
     
 
 document.getElementsByClassName("d-none")[0].style.display = "none";
-documbent.getElementsByClassName("b-none")[0].style.display = "none";
 
-window.addEventListener('load', function(){
+
+
+    var errorDiv = getElementByXpath("/html/body/div[4]/div/div/div");
+    var errorText = getElementByXpath("/html/body/div[4]/div/div/div/h4");
+    
     var queryString = window.location.search;
     var urlParams = new URLSearchParams(queryString);
     var test = urlParams.get('test');
 
 if(test == "failed"){
-    errorDiv.className ="d-block";
+    errorDiv.className ="b-block";
     
     errorText.textContent = 'Credentiale Invalide';
     document.getElementsByClassName("b-block")[0].style.backgroundColor = "red";
 }else{
-    
+    documbent.getElementsByClassName("b-none")[0].style.display = "none";
 }
-})
+
+
+
 
 
